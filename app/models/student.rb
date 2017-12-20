ddc1535ff9644fb38d6b010a4476f30b70ff0396
Mashiyyat DelosSantos <delossantos.mash@gmail.com>
2017-12-20 21:23:24 +0800

    Seeds

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,5 @@ class Student < ApplicationRecord
   extend FriendlyId
   friendly_id :lrn, use: [:slugged , :finders]
   validates :lrn , presence: true , uniqueness: true
-  has_many :grades , dependent: :destroy
+  # has_many :grades , dependent: :destroy
 end
