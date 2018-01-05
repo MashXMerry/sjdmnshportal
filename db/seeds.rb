e67a54156174f98058c0c9873a6d2ffb749caf45
Mashiyyat DelosSantos <delossantos.mash@gmail.com>
2018-01-06 00:02:14 +0800

    Run seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,9 @@
 	Student.delete_all
 	Section.delete_all
 	Subject.delete_all
+	News.delete_all
+	Announcement.delete_all
+	Event.delete_all
+	Grading.delete_all
 	Administrator.delete_all
-	Administrator.create(email: 'delossantos.mash@gmail.com', password: 'mvds.com', firstname: 'Mashiyyat' , middlename: 'Villasenor', lastname:'Delos Santos', role: 'registrar')
+	Administrator.create(username:'Mashiyyat' , email: 'delossantos.mash@gmail.com', password: 'mvds.com', firstname: 'Mashiyyat' , middlename: 'Villasenor', lastname:'Delos Santos', role: 'registrar')
