581edc73778b04ddfbfdf8d52fd665448602d07b
Mashiyyat DelosSantos <delossantos.mash@gmail.com>
2018-01-06 00:17:30 +0800

    Updated 12:17pm JAN/6

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -6,7 +6,6 @@ class Administrator < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/       
-  validates :username , presence: true , uniqueness: true
-  # validates :password , presence: true , uniqueness: true
-  validates :email , presence: true , uniqueness: true
+  validates :username , uniqueness: true
+  validates :email , uniqueness: true
 end
